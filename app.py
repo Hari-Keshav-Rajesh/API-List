8aefd00f778d6cc5aa7c910880f562d5ef0523e8
unknown <harikeshav.rajesh@gmail.com>
2023-09-14 00:30:16 +0530

    Added logout feature and adding table for user on register

--- app.py
+++ app.py
@@ -1,3 +1,22 @@
 from fastapi import FastAPI
 
+
 app = FastAPI()
+
+import mysql.connector
+from mysql.connector import Error
+
+from private import *
+
+try:
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password=password_my_sql,
+        database="API_LIST"
+    )
+
+    if connection.is_connected():
+        print("Connected to MySQL database")
+except Error as e:
+    print(f"Error: {e}")
--- login.py
+++ login.py
@@ -1,37 +1,18 @@
-from app import app
-
+from app import *
 from pydantic import BaseModel
-
-import mysql.connector
-from mysql.connector import Error
-
-from private import *
-
-try:
-    connection = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password=password_my_sql,
-        database="API_LIST"
-    )
-
-    if connection.is_connected():
-        print("Connected to MySQL database")
-except Error as e:
-    print(f"Error: {e}")
 
 class Login_Request(BaseModel):
     username: str
     password: str
 
-login = False
-
-def isLogin():
-    if login==True:
-        return True
+app.login = {'status':False,'user':""}
 
 @app.post("/login")
-def login(login_request: Login_Request):
+def log_in(login_request: Login_Request):
+
+    if(app.login['status']==True):
+        return {'Message':'Already Logged In','User':app.login['user']}
+
     username = login_request.username
     password = login_request.password
     query1 = "SELECT COUNT(*) FROM accounts WHERE Username = %s"
@@ -59,25 +40,68 @@ def login(login_request: Login_Request):
             cursor.close()
         
         if(password == pass_word):
-            login = True
-            return {'Message': 'Logged In Successfully'}
+            app.login['status'] = True
+            app.login['user'] = f"{username}"
+            return {'Message': 'Logged In Successfully','User':app.login['user'],'login':app.login['status']}
         else:
             return {'Message':'Password Incorrect'}
+        
+@app.post("/logout")
+def logout():
+    if(app.login['status']==True):
+        app.login['status'] = False
+        app.login['user'] = ""
+        return {"Message":"Logged Out"}
+    return{"Message":"Not logged in"}
+
 
 @app.post("/register")
 def register(login_register: Login_Request):
+
+    if(app.login['status']==True):
+        return {'Message':'Already Logged In','User':app.login['user']}
+    
     username = login_register.username
     password = login_register.password
 
-    query1 = "INSERT INTO accounts (Username,Password) VALUES (%s,%s)"
-    
+    query1 = "SELECT COUNT(*) FROM accounts WHERE Username = %s"
+
     try:
         with connection.cursor() as cursor:
-            cursor.execute(query1,(username,password))
-            connection.commit()
-            return {'Message':'Account Created'}
+            cursor.execute(query1, (username,))
+            count = cursor.fetchone()[0]
     except mysql.connector.Error as e:
         return {'error': 'Database error', 'details': str(e)}
     finally:
-        cursor.close()
+        cursor.close() 
+    
+    if(count>0):
+        return{"Message":"Username already exists"}
+    else:
+        query2 = "INSERT INTO accounts (Username,Password) VALUES (%s,%s)"
+        query3 = f"CREATE TABLE {username} (Task VARCHAR(50),Type VARCHAR(10))"
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute(query2,(username,password))
+                cursor.execute(query3)
+                connection.commit()
+                return {'Message':'Account Created'}
+        except mysql.connector.Error as e:
+            return {'error': 'Database error', 'details': str(e)}
+        finally:
+            cursor.close()
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
 
